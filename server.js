99bcf446c0dfaa715b627bdcad2b2eb03c094f86
Fernanda Constância <91707875+fernandaconstancia@users.noreply.github.com>
2022-09-28 21:32:18 -0300

    initial commit

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
+const path = require("path");
+const cors = require("cors");
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
-const cors = require("cors");
-const port = process.env.PORT || 3001;
 
 const app = jsonServer.create();
-const router = jsonServer.router("db.json");
+// const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const port = process.env.PORT || 8080;
 
 app.db = router.db;
 
 const rules = auth.rewriter({
-  users: 600,
+  "/users*": "/600/users$1",
+  "/products*": "/640/products$1",
 });
 
 app.use(cors());
@@ -19,5 +22,3 @@ app.use(router);
 app.listen(port);
 
 console.log("Server is running on port:", port);
-
-/* A senha do Kenzinho é 123456 */
